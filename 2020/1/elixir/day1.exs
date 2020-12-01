b429e4b4d0da558be6a8df8a4fc92517f1a3a035
remicmacs <rembourgeon@gmail.com>
2020-12-01 23:56:22 +0100

    Solution to 2020/1/2
    Really inefficient

--- 2020/1/elixir/day1.exs
+++ 2020/1/elixir/day1.exs
@@ -6,30 +6,48 @@ defmodule Helpers do
 		|> Enum.to_list
 	end
 
-#	def find_match(ref, list), do: find_match(ref, list)
-	def find_match(ref, []), do: []
-	def find_match(ref, [head | tail])do
-		case ref + head do
-			2020 -> [ref, head]
-			_ -> find_match(ref, tail)
+	def find_correct_pair([[a,b] | tail]) do
+		case a + b do
+			2020 -> [a,b]
+			_ -> find_correct_pair(tail)
 		end
 	end
 
-	def find_matches(entries), do: find_matches([], entries)
-	defp find_matches(processed, []), do: processed
-	defp find_matches(processed, [head | tail]) do
-		find_matches([find_match(head, tail) | processed], tail)
+	def find_correct_triple([[a,b,c] | tail]) do
+		case a + b + c do
+			2020 -> [a,b,c]
+			_ -> find_correct_triple(tail)
+		end
 	end
+
+	def find_all_pairs(entries), do: find_all_pairs([], entries)
+	defp find_all_pairs(pairss, []), do: Enum.concat(pairss)
+	defp find_all_pairs(pairss, [head | tail]) do
+		pairs = for x <- tail, do: [head, x]
+		find_all_pairs([pairs | pairss], tail)
+	end
+
+	def find_all_triples(entries), do: find_all_triples([], entries)
+	defp find_all_triples(tripless, []), do: Enum.concat(tripless)
+	defp find_all_triples(tripless, [head | tail]) do
+		triples = for [a,b] <- find_all_pairs(tail), do: [a, b, head]
+		find_all_triples([triples | tripless], tail)
+	end
+
 end
 
 
 entries = Helpers.filtered_lines(:stdio)
 
-IO.inspect(entries)
-res = Helpers.find_matches(entries)
-IO.inspect(res)
-res = Enum.filter(res, fn x -> x != [] end)
-IO.inspect(res)
-[a,b] = hd res
-mul = a * b
-IO.inspect(mul)
+pairs = Helpers.find_all_pairs(entries)
+IO.inspect(pairs)
+correct_pair = Helpers.find_correct_pair(pairs)
+IO.inspect(correct_pair)
+[a,b] = correct_pair
+IO.inspect(a*b)
+
+triples = Helpers.find_all_triples(entries)
+correct_triple = Helpers.find_correct_triple(triples)
+IO.inspect(correct_triple)
+[a,b,c] = correct_triple
+IO.inspect(a*b*c)
