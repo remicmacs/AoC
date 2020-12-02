dd31a959eed54fe431d4d4bb433c35337f11f898
remicmacs <rembourgeon@gmail.com>
2020-12-02 09:11:41 +0100

    2020/2/2 solved

--- 2020/2/elixir/day2.exs
+++ 2020/2/elixir/day2.exs
@@ -5,16 +5,16 @@ defmodule Helpers do
 	end
 
 	def split_password_and_policy(string) do
-		[policy_range, policy_char, pw] = string
+		[policy_indices, policy_char, pw] = string
 			|> String.split(" ")
 		policy_char = String.replace(policy_char, ":", "")
-		policy_range = policy_range
+		policy_indices = policy_indices
 			|> String.split("-")
 			|> Enum.map(&String.to_integer/1)
 		%{
 			policy:
 			%{
-				range: policy_range,
+				indices: policy_indices,
 				char: policy_char
 			},
 			pw: pw
@@ -27,19 +27,40 @@ defmodule Helpers do
 	end
 
 	def pw_respects_policy?(%{policy: policy, pw: pw}) do
-		%{range: prange, char: pchar} = policy
+		%{indices: prange, char: pchar} = policy
 		count = count_substring(pw, pchar)
 		[min, max] = prange
 		(count <= max and count >= min)
 	end
+
+	def pw_respects_other_policy?(%{policy: policy, pw: pw}) do
+		%{indices: pindices, char: pchar} = policy
+		[first, second] =  pindices
+			|> Enum.map(&(&1 -1))
+			|> Enum.map(&(is_char_at?(pw, pchar, &1)))
+			# |> IO.inspect
+		(first or second) and not(first and second)
+	end
+
+	def is_char_at?(str, char, index) do
+		String.at(str, index) == char
+	end
+
 end
 
-IO.stream(:stdio, :line)
+pws_policies = IO.stream(:stdio, :line)
 	|> Helpers.parse
 	|> Enum.to_list
 	|> Enum.map(&Helpers.split_password_and_policy/1)
-	# |> IO.inspect
+
+pws_policies
 	|> Enum.filter(&Helpers.pw_respects_policy?/1)
+	# |> IO.inspect
+	|> length
+	|> IO.puts
+
+pws_policies
+	|> Enum.filter(&Helpers.pw_respects_other_policy?/1)
 	# |> IO.inspect
 	|> length
 	|> IO.puts
