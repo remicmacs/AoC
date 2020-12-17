743e920ae6a9153dba5315895c1f051d1c62e589
remicmacs <rembourgeon@gmail.com>
2020-12-17 08:03:53 +0100

    2020/16/2 Solution
    Late solution, but better late than never

--- 2020/16/elixir/day16.exs
+++ 2020/16/elixir/day16.exs
@@ -31,6 +31,30 @@ defmodule Run do
 end
 
 defmodule Helpers do
+	def filter_out_matching(candidates, already_matching) do
+		Enum.filter(candidates, fn candidate -> not (candidate in already_matching) end )
+	end
+	def find_matching_fields(fields, found_matches) do
+		already_matching = Map.values(found_matches)
+
+		new_fields = fields
+			|> Enum.map(
+				fn candidates ->
+					filter_out_matching(candidates, already_matching) end
+			)
+
+		indexed_fields = Enum.with_index(new_fields)
+
+		found_matches = indexed_fields
+			|> Enum.filter(fn {candidates, index} -> length(candidates) == 1 end)
+			|> Enum.reduce(found_matches, fn {[sole_candidate], index}, acc -> Map.put(acc, index, sole_candidate) end)
+
+		if length(Map.keys(found_matches)) == length(fields) do
+			found_matches
+		else
+			find_matching_fields(new_fields, found_matches)
+		end
+	end
 
 	def verify_rule?(rule, value) do
 		[lower, higher | _nothing] = rule
@@ -65,6 +89,32 @@ defmodule Helpers do
 			# |> IO.inspect
 			|> Enum.concat
 			|> Enum.sum
+	end
+
+	def values_validate_constraint(zipped_values, constraint) do
+		# IO.inspect(constraint, label: "constraint")
+		zipped_values
+			|> Enum.map(fn value -> validate_constraint(constraint, value) end)
+			# |> IO.inspect
+			|> Enum.all?(fn value -> value end)
+	end
+
+	def find_validating_constraint(zipped_values, constraints) do
+		constraints
+			|> Enum.filter(fn constraint -> values_validate_constraint(zipped_values, constraint) end)
+			|> Enum.map(fn {constraint_name, _rules} -> constraint_name end )
+	end
+
+	def zip_nearby_tickets_values(input) do
+		input.nearby_tickets
+			|> Enum.zip
+			|> Enum.map(&Tuple.to_list/1)
+	end
+
+	def filter_out_invalid_tickets(input) do
+		valid_tickets = input.nearby_tickets
+			|> Enum.filter(fn ticket -> [] == find_infringing_values(ticket, input.constraints) end)
+		%{input | nearby_tickets: valid_tickets}
 	end
 
 	def parse_rule(rule_str) do
@@ -132,7 +182,21 @@ defmodule Helpers do
 	end
 
 	def process_pt2(file_name) do
-		file_name
+		input = file_name
+			|> file_name_load
+			|> parse_input
+			|> filter_out_invalid_tickets
+		zipped_nearby_ticket_values = zip_nearby_tickets_values(input)
+		zipped_nearby_ticket_values
+			|> Enum.map(
+				fn zipped_value -> find_validating_constraint(zipped_value, input.constraints) end
+			)
+			|> find_matching_fields(%{})
+			|> Enum.to_list
+			|> Enum.zip(input.ticket)
+			|> Enum.filter(fn {{index, key}, ticket_value} -> Regex.match?(~r/^departure.*$/, key) end)
+			|> Enum.map(fn {{index, key}, ticket_value} -> ticket_value end)
+			|> Enum.reduce(1, fn val, acc -> val * acc end)
 	end
 end
 
