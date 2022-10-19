69722dd0f613bee86290a4cee86d9085563e75a4
remicmacs <remicmacs@protonmail.com>
2022-10-19 20:53:06 +0200

    feat(run): parse cmdline args in separated fn

--- aoc_rust/src/main.rs
+++ aoc_rust/src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs;
 use std::process;
 
-fn main() {
+fn parse_cmdline_args() -> Result<String, &'static str> {
     let args: Vec<String> = env::args().collect();
     let file_path: String;
     match args.len() {
@@ -11,48 +11,46 @@ fn main() {
             let year: u32 = match args[1].parse::<u32>() {
                 Ok(number) => {
                     if number < 2022 && number > 2014 {
-                        println!("Valid year {number}");
                         number
+                    } else {
+                        return Err("Please chose a year in [2015-2021] range");
                     }
-                    else {
-                        println!("Please chose a year in [2015-2021] range");
-                        process::exit(1);
-                    }
-                },
-                _ => {
-                        println!("Expected a number year !");
-                        process::exit(1);
                 }
+                _ => return Err("Expected a number year !"),
             };
 
             let day_nb: u32 = match args[2].parse::<u32>() {
                 Ok(number) => {
                     if number < 26 && number > 0 {
-                        println!("Valid day_nb {number}");
                         number
+                    } else {
+                        return Err("Please chose a day in [1-25] range");
                     }
-                    else {
-                        println!("Please chose a day in [1-25] range");
-                        process::exit(1);
-                    }
-                },
-                _ => {
-                        println!("Expected a number day !");
-                        process::exit(1);
                 }
+                _ => return Err("Expected a number day !"),
             };
 
             let input_file = &args[3];
 
             file_path = format!("../{year}/day{day_nb}/inputs/{input_file}");
-        },
+        }
         _ => {
-            println!("This program expects 3 positional args <year>, <day_nb> and <input_file>");
-            // Generic error code
-            process::exit(1);
-        },
+            return Err("This program expects 3 positional args <year>, <day_nb> and <input_file>")
+        }
     }
 
+    Ok(file_path)
+}
+
+fn main() {
+    let file_path: String;
+    match parse_cmdline_args() {
+        Ok(valid_str) => file_path = valid_str,
+        Err(err_msg) => {
+            println!("{err_msg}");
+            process::exit(1);
+        }
+    };
     println!("Opening file {file_path}");
     let contents = fs::read_to_string(file_path).expect("Unexpected error while reading the file");
 
