09781cbb3cb20b63049194c42fffb62f9501d49f
remicmacs <remicmacs@protonmail.com>
2022-10-19 21:43:18 +0200

    feat(rust): move solver logic to separated fns

--- aoc_rust/src/main.rs
+++ aoc_rust/src/main.rs
@@ -42,6 +42,30 @@ fn parse_cmdline_args() -> Result<String, &'static str> {
     Ok(file_path)
 }
 
+fn final_floor(input: &str) -> isize {
+    let tokens: Vec<char> = input.chars().collect();
+    tokens.iter().fold(0, |acc, x| if *x == '(' { acc + 1 } else { acc - 1 }) as isize
+}
+
+fn first_basement_floor(input: &str) -> usize {
+    let tokens: Vec<char> = input.chars().collect();
+
+    let mut first_basement_floor: usize = 0;
+    let mut floor = 0;
+    for (i, token) in tokens.iter().enumerate() {
+        match token {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => continue,
+        }
+        if first_basement_floor == 0 && floor < 0 {
+            first_basement_floor = i + 1;
+        }
+    }
+
+    first_basement_floor
+}
+
 fn main() {
     let file_path: String;
     match parse_cmdline_args() {
@@ -51,23 +75,11 @@ fn main() {
             process::exit(1);
         }
     };
+
     println!("Opening file {file_path}");
     let contents = fs::read_to_string(file_path).expect("Unexpected error while reading the file");
 
-    let tokens: Vec<char> = contents.chars().collect();
-
-    let mut first_reach_basement = 0;
-    let mut floor = 0;
-    for (i, token) in tokens.iter().enumerate() {
-        match token {
-            '(' => floor += 1,
-            ')' => floor -= 1,
-            _ => continue,
-        }
-        if first_reach_basement == 0 && floor < 0 {
-            first_reach_basement = i + 1;
-        }
-    }
-
-    println!("{floor}\n{first_reach_basement}");
+    let final_floor = final_floor(&contents);
+    let first_basement_floor = first_basement_floor(&contents);
+    println!("{final_floor}\n{first_basement_floor}");
 }
