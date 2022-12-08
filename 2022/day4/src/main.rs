985d86c662527c3755884e0dd48eb44a0eb5f514
remicmacs <remicmacs@protonmail.com>
2022-12-08 08:40:35 +0100

    feat(rust): 2022/4 part 2 solution

--- 2022/day4/src/main.rs
+++ 2022/day4/src/main.rs
@@ -35,5 +35,34 @@ fn main() -> Result<()> {
 
     println!("{total}");
 
+    let total: usize = input
+        // Split input by lines
+        .lines()
+        .map(|line| {
+            let asst_pair: Vec<HashSet<usize>> = line
+                // Each pair is separated by a colon
+                .split(",")
+                .map(|assignment| {
+                    let bounds: Vec<usize> = assignment
+                        // In each pair the assignement range is separated by a dash
+                        .split("-")
+                        .map(|bound| {
+                            usize::from_str(bound).expect("The bound is expected to be an int")
+                        })
+                        .collect();
+                    let lower_bound = bounds[0];
+                    let upper_bound = bounds[1];
+                    HashSet::from_iter(lower_bound..=upper_bound)
+                })
+                .collect();
+            let first_asst = &asst_pair[0];
+            let second_asst = &asst_pair[1];
+            !(first_asst.is_disjoint(&second_asst))
+        })
+        .filter(|x| *x)
+        .count();
+
+    println!("{total}");
+
     Ok(())
 }
