a9bebb155a16c3a13670c22969e915af7bfca82d
remicmacs <remicmacs@protonmail.com>
2022-12-29 18:07:21 +0100

    feat(rust): 2022/6 part 2 solution

--- 2022/day6/src/main.rs
+++ 2022/day6/src/main.rs
@@ -1,5 +1,33 @@
+use std::collections::{HashSet, VecDeque};
 
-fn find_marker(s: &str) -> usize {
+fn find_start_of_message_marker(s: &str) -> usize {
+    let mut ring: VecDeque<char> = VecDeque::with_capacity(14);
+
+    for (i, c) in s.chars().enumerate() {
+        // just fill the buffer for now
+        if i < 14 {
+            ring.push_back(c);
+            continue;
+        }
+
+        // check if no duplicates in current buffer
+        let mut charset: HashSet<char> = HashSet::new();
+        for ch in ring.iter() {
+            charset.insert(*ch);
+        }
+        let ok = charset.len() == 14;
+        // if no duplicates in buffer, return
+        if ok {
+            return i;
+        }
+        ring.pop_front();
+        ring.push_back(c)
+    }
+
+    return 0;
+}
+
+fn find_start_of_packet_marker(s: &str) -> usize {
     let mut first: char;
     let mut second: char;
     let mut third: char;
@@ -44,10 +72,17 @@ fn find_marker(s: &str) -> usize {
 
 fn main() {
     let input = include_str!("../inputs/main.0");
-    assert_eq!(7, find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
-    assert_eq!(5, find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"));
-    assert_eq!(6, find_marker("nppdvjthqldpwncqszvftbrmjlhg"));
-    assert_eq!(10, find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
-    assert_eq!(11, find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
-    println!("{}", find_marker(input));
+    assert_eq!(7, find_start_of_packet_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+    assert_eq!(5, find_start_of_packet_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+    assert_eq!(6, find_start_of_packet_marker("nppdvjthqldpwncqszvftbrmjlhg"));
+    assert_eq!(10, find_start_of_packet_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+    assert_eq!(11, find_start_of_packet_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+    println!("{}", find_start_of_packet_marker(input));
+
+    assert_eq!(19, find_start_of_message_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+    assert_eq!(23, find_start_of_message_marker("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+    assert_eq!(23, find_start_of_message_marker("nppdvjthqldpwncqszvftbrmjlhg"));
+    assert_eq!(29, find_start_of_message_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+    assert_eq!(26, find_start_of_message_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+    println!("{}", find_start_of_message_marker(input));
 }
